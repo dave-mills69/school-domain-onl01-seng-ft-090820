581966df521e8ab28786601590362ac5dd22e6ca
David Mills <david_mills109@yahoo.com>
2020-09-18 15:24:31 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,16 @@
 # code here!
+require 'pry'
+class School
+  attr_reader :roster 
+  
+  def initialize(name)
+    @roster = {}
+  end
+  
+  def add_student(name, grade)
+   #binding.pry
+    @roster[grade] = [] if !@roster[grade]
+    @roster[grade] << name 
+  end
+end
+ 
